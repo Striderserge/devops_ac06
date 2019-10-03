bd4066220b75f68d751e416bd50e4d7fa6df2342
Striderserge <sergio.barros@aluno.faculdadeimpacta.com.br>
2019-10-03 20:58:48 -0300

    corrido formatacao

--- com/ope/calcula_parcela.py
+++ com/ope/calcula_parcela.py
@@ -1,10 +1,12 @@
-def valorPagamento(valor, diasAtraso):
+def valorpagamento(valor, diasatraso):
+    """verifica o valor a ser pago de uma prestação"""
 
-    if (valor < 0):
+    if valor < 0:
+        '''verifica se o valor é zero'''
         return None
-    if (diasAtraso > 0):
+    if diasatraso > 0:
+        '''verifica se o valor de atraso é maior que 0'''
         multa = valor * 0.03
-        adicionalAtraso = valor * (diasAtraso * 0.01)
-        return valor + multa + adicionalAtraso
-    else:
-        return valor
+        adicionalatraso = valor * (diasatraso * 0.01)
+        return valor + multa + adicionalatraso
+    return valor
--- com/ope/test/test_calcula_parcela.py
+++ com/ope/test/test_calcula_parcela.py
@@ -1,12 +1,11 @@
 import pytest
-from com.ope.calcula_parcela import valorPagamento
+from com.ope.calcula_parcela import valorpagamento
 
 def test_calcula_parcela1():
-    assert valorPagamento(10,5) == 10.8, "Deverá ser 10.8"
+    assert valorpagamento(10, 5) == 10.8, "Deverá ser 10.8"
 
 def test_calcula_parcela2():
-    assert valorPagamento(-5,0) == None, "Deverá ser None"
-    
-def test_calcula_parcela3():
-    assert valorPagamento(10,0) == 10, "Deverá ser 10"
+    assert valorpagamento(-5, 0) == None, "Deverá ser None"
 
+def test_calcula_parcela3():
+    assert valorpagamento(10, 0) == 10, "Deverá ser 10"
