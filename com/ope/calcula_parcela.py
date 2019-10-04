61aeca0faee42407520f8919405f0030922a1a6a
Striderserge <53881982+Striderserge@users.noreply.github.com>
2019-10-03 21:04:17 -0300

    Update calcula_parcela.py
    alterado

--- com/ope/calcula_parcela.py
+++ com/ope/calcula_parcela.py
@@ -1,11 +1,13 @@
+"""sistemas valor pagamento"""
+
 def valorpagamento(valor, diasatraso):
     """verifica o valor a ser pago de uma prestação"""
 
     if valor < 0:
-        '''verifica se o valor é zero'''
+        """verifica se o valor é zero"""
         return None
     if diasatraso > 0:
-        '''verifica se o valor de atraso é maior que 0'''
+        """verifica se o valor de atraso é maior que 0"""
         multa = valor * 0.03
         adicionalatraso = valor * (diasatraso * 0.01)
         return valor + multa + adicionalatraso
