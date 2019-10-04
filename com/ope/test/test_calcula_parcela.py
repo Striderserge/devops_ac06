210869a7c5cbe2ee4a052d6e3a241a0370705c56
Striderserge <53881982+Striderserge@users.noreply.github.com>
2019-10-03 21:10:13 -0300

    Update test_calcula_parcela.py
    alterado o teste

--- com/ope/test/test_calcula_parcela.py
+++ com/ope/test/test_calcula_parcela.py
@@ -1,11 +1,15 @@
-import pytest
+"""teste do calcula calcula parcela"""
+
 from com.ope.calcula_parcela import valorpagamento
 
 def test_calcula_parcela1():
+    """teste 1"""
     assert valorpagamento(10, 5) == 10.8, "Deverá ser 10.8"
 
 def test_calcula_parcela2():
-    assert valorpagamento(-5, 0) == None, "Deverá ser None"
+    """teste 2"""
+    assert valorpagamento(-5, 0) is None, "Deverá ser None"
 
 def test_calcula_parcela3():
+    """teste 3"""
     assert valorpagamento(10, 0) == 10, "Deverá ser 10"
