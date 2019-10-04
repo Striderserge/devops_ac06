af83bfda7e035484ed2124373c0bc49b04099158
Striderserge <53881982+Striderserge@users.noreply.github.com>
2019-10-03 21:23:28 -0300

    Update conta_corrente.py
    corrigido identação

--- com/ope/conta_corrente.py
+++ com/ope/conta_corrente.py
@@ -1,28 +1,21 @@
 """programa conta corrente"""
+
 class Contacorrente:
 
     def __init__(self, numero, nomecorrentista, saldo=0.0):
-      """recebe parametro"""
+        """recebe parametro"""
         self.numero = numero
         self.alterarnome(nomecorrentista)
         self.saldo = saldo
 
     def alterarnome(self, nomecorrentista):
-      """altera o nome"""
+        """altera o nome"""
         self.nomecorrentista = nomecorrentista
 
     def deposito(self, valor):
-      """faz o deposito"""
+        """faz o deposito"""
         self.saldo += valor
 
     def saque(self, valor):
-      """faz o saque"""
+        """faz o saque"""
         self.saldo -= valor
-
-# TESTE DA CLASSE
-'''
-conta = Contacorrente(100, 'Tales')
-conta.deposito(100)
-print conta.saldo
-conta.saque(200)
-print conta.saldo'''
