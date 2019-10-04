bf1ebf02dda14f48e00720ca0bf2ebb688d4d19d
Striderserge <53881982+Striderserge@users.noreply.github.com>
2019-10-03 21:30:11 -0300

    Update test_convertehora.py
    alteracao semi final

--- com/ope/test/test_convertehora.py
+++ com/ope/test/test_convertehora.py
@@ -5,16 +5,16 @@ from com.ope import converte_hora
 
 def test_hora1():
     """teste1"""
-    assert converte_hora.converteHora(25, 0) is None, "Deve retornar nulo"
+    assert converte_hora.convertehora(25, 0) is None, "Deve retornar nulo"
 
 def test_hora2():
     """teste2"""
-    assert converte_hora.converteHora(10, 10) == '10:10 AM', "deve ser 10:10AM"
+    assert converte_hora.convertehora(10, 10) == '10:10 AM', "deve ser 10:10AM"
 
 def test_hora3():
     """teste3"""
-    assert converte_hora.converteHora(23, 10) == '11:10 PM', "deve ser 11:10PM"
+    assert converte_hora.convertehora(23, 10) == '11:10 PM', "deve ser 11:10PM"
 
 def test_hora4():
     """teste4"""
-    assert converte_hora.converteHora(0, 10) == '12:10 AM', "deve ser 12:10AM"
+    assert converte_hora.convertehora(0, 10) == '12:10 AM', "deve ser 12:10AM"
