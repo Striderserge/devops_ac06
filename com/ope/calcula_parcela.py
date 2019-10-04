40246e443e4ab51fb99a2110d192a5c01a293c9c
Striderserge <53881982+Striderserge@users.noreply.github.com>
2019-10-03 21:11:13 -0300

    Update calcula_parcela.py
    retirado string

--- com/ope/calcula_parcela.py
+++ com/ope/calcula_parcela.py
@@ -4,10 +4,8 @@ def valorpagamento(valor, diasatraso):
     """verifica o valor a ser pago de uma prestação"""
 
     if valor < 0:
-        """verifica se o valor é zero"""
         return None
     if diasatraso > 0:
-        """verifica se o valor de atraso é maior que 0"""
         multa = valor * 0.03
         adicionalatraso = valor * (diasatraso * 0.01)
         return valor + multa + adicionalatraso
